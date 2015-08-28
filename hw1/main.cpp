0e86f528f53636b7e4d6ef2f462112e94c617757
Lisa Churchman <Lisa@wrls-108-230.sonoma.edu>
2015-08-28 14:18:29 -0700

    Initial Commit

---
+++ hw1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw1
+//
+//  Created by Lisa Churchman on 8/28/15.
+//  Copyright (c) 2015 Lisa Churchman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
